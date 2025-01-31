08265116584b24cc718fc74bec31a2effbff519a
Diego Omitto <diego.omitto@gmail.com>
2025-01-31 14:02:30 -0500

    Add/update common modules and remove generated files

--- iocBoot/iocplc_ipr/st.cmd
+++ iocBoot/iocplc_ipr/st.cmd
@@ -7,6 +7,16 @@
 
 cd "${TOP}"
 
+#--
+epicsEnvSet("ENGINEER",  "Pedro Rodriguez")
+epicsEnvSet("LOCATION",  "")
+epicsEnvSet("WIKI", "")
+#-- 
+epicsEnvSet("IOCNAME", "mpex-plc-ipr")
+epicsEnvSet("IOC", "iocplc-ipr")
+#--
+
+
 ## Register all support components
 dbLoadDatabase "dbd/plc_ipr.dbd"
 plc_ipr_registerRecordDeviceDriver pdbbase
@@ -19,10 +29,11 @@ EIP_verbosity(7)
 
 drvEtherIP_define_PLC("pshcs_ipr", "10.112.2.11", 2)
 
-
 ## Load record instances
 #dbLoadTemplate "db/user.substitutions"
 dbLoadRecords "db/plc_ipr.db", "PLC=pshcs_ipr"
+
+#asSetFilename("${ASCF}/ics-default.acf")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
